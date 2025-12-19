f189e99acecf4465663d6c101c4b0f8c4b6b22dd
haze103 <112647323+haze103@users.noreply.github.com>
2025-12-19 22:47:15 +0800

    passed

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 'use strict';
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const bcrypt      = require('bcrypt');
+const cors        = require('cors'); // 1. Add this requirement
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+
 const app         = express();
-fccTesting(app);
+
+app.use(cors()); // 2. Use the middleware here
+fccTesting(app); // 3. This MUST come after app.use(cors())
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 //END_SYNC
 
 
